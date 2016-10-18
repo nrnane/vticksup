83a7d5c811660c538a250634f1fc9895261547e1
Nagaraju <nrnane@gmail.com>
2016-10-18 19:22:12 +0530

    backup deleted

--- backup/application/views/blocks/flash_msg.php
+++
@@ -1,5 +0,0 @@
-<?php
-if($this->session->flashdata('msg')):
-    echo '<div class="alert alert-success">'.$this->session->flashdata('msg').'</div>';
-endif;
-?>
--- backup/application/views/comman/template1.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('comman/header.php'); ?>
-<div class="container clearfix">
-<?php $this->load->view($page); ?>
- </div> <!--End Container -->
-<?php $this->load->view('comman/footer.php'); ?>
